61ca88e2def01a55c711bd9aa77f559b1341c080
Richika039 <laurence.barthelemy@condorcet.be>
2025-08-08 23:47:45 +0200

    Exo supplémentaire

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Écrivez votre nom :");
+            string n = Console.ReadLine();
+
+            Console.WriteLine("Écrivez votre âge :");
+            string a = Console.ReadLine();
+
+            Console.WriteLine(string.Format("Vous vous appelez : {0}, vous avez : {1} ans, confirmez-vous cette information ?", n, a));
+
+            Pause();
+        }
+
+        private static void Pause()
+        {
+            Console.WriteLine("Appuyez sur une touche pour continuer...");
+            Console.ReadKey(true);
+        }
+    }
+}
