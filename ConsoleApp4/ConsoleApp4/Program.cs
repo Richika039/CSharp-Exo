be89470bb84e983e056f1ff119c484e927523b12
Richika039 <laurence.barthelemy@condorcet.be>
2025-08-08 23:54:35 +0200

    Update Program.cs

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp1
 {
@@ -6,14 +6,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // byte age = 23;
+            // float f = 0.0025f;
+            // decimal d = 0.025m;
+            // string texte = "blablabla";
+            // texte = texte + " totototot" + age.ToString() + "" + f;
             Console.WriteLine("Écrivez votre nom :");
             string n = Console.ReadLine();
 
             Console.WriteLine("Écrivez votre âge :");
             string a = Console.ReadLine();
+            a = a + 1;
 
             Console.WriteLine(string.Format("Vous vous appelez : {0}, vous avez : {1} ans, confirmez-vous cette information ?", n, a));
+            //Console.WriteLine("Vous vous appelez " + s + ", confirmez vous cette information ?");
 
+            // Affiche le texte final
+            //Console.WriteLine("Texte final : " + texte);
+
+            // Appel de la pause
             Pause();
         }
 
